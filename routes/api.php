2909395d5f7adb3ddba74145508ed3a424eb742c
ajeeharu <ajeeharu@gmail.com>
2026-04-23 19:28:28 +0900

    localhost:8080/public で表示するように修正

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
-// routes/api.php
-use App\Http\Controllers\Api\PostController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// 一括でRESTfulなエンドポイントを定義
-Route::apiResource('posts', PostController::class);
+// この定義があれば /api/users でアクセス可能
+Route::get('/users', function () {
+    return response()->json([
+        ['id' => 1, 'name' => 'User 1'],
+        ['id' => 2, 'name' => 'User 2']
+    ]);
+});
