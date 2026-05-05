4497b92a386795910dae865528b769faf239b9a5
ajeeharu <ajeeharu@gmail.com>
2026-05-05 21:22:57 +0900

    user Factory Mode設定、created_at updated_at を明示的に設定

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,15 +1,22 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory; // 1. これを追加
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    use HasFactory, Notifiable;
     // 主キーの型を指定
     protected $keyType = 'string';
 
     // オートインクリメントを無効化
     public $incrementing = false;
+
+    // タイムスタンプの自動更新を有効化（デフォルトでtrueなので、書かなくてもOKです）
+    public $timestamps = true;
 
     protected $fillable = [
         'id', // subをここに入れる
