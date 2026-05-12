d0e82bc3ba15a7f10d9e8b34a49b705677860261
ajeeharu <ajeeharu@gmail.com>
2026-05-12 12:07:16 +0900

    S3への署名付きURLの取得APIを実装

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\VideoUploadController;
 
 // api/users に対して一括でCRUDルートを割り当て
 Route::apiResource('users', UserController::class);
+// 動画アップロード用の署名付きURL取得エンドポイント
+Route::post('/get-presigned-url', [VideoUploadController::class, 'getPresignedUrl']);
